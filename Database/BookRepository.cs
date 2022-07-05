0207ed6eb4209528f27acf913c876bfd5eff3dc4
Caleb Vandenburg <47614130+CalebVandenburg@users.noreply.github.com>
2022-07-05 16:56:28 -0400

    Can now write a review in app

--- Database/BookRepository.cs
+++ Database/BookRepository.cs
@@ -26,12 +26,12 @@ namespace HomeLibraryManager.Database
         }
         public IEnumerable<Review> GetReviews()
         {
-            return databaseContext.Reviews.Include(x=>x.Book);
+            return databaseContext.Reviews.Include(x => x.Book);
         }
         public Book EditBook(BookEditModel bookEdits)
         {
             var book = GetBooks().Where(x => x.BookId == bookEdits.BookID).FirstOrDefault();
-            if(book != null)
+            if (book != null)
             {
                 book.EditionNotes = bookEdits.EditionNotes;
                 book.IsFirstEdition = bookEdits.IsFirstEdition == "on" ? true : false;//bookEdits.IsFirstEdition;
@@ -53,7 +53,7 @@ namespace HomeLibraryManager.Database
         public bool DeleteBook(int bookID)
         {
             var book = GetBooks().Where(x => x.BookId == bookID).FirstOrDefault();
-            if(book != null)
+            if (book != null)
             {
                 databaseContext.ChangeTracker.Clear();
                 databaseContext.Attach(book).State = EntityState.Deleted;
@@ -65,11 +65,25 @@ namespace HomeLibraryManager.Database
         public bool DeleteBooks(List<int> bookIDs)
         {
             var books = GetBooks().Where(x => bookIDs.Contains(x.BookId)).ToList();
-            
+
             if (books != null && books.Count() > 0)
             {
                 databaseContext.ChangeTracker.Clear();
                 databaseContext.RemoveRange(books);
+                databaseContext.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+        public bool CreateReview(Review review)
+        {
+            var book = GetBooks().Where(x => x.BookId == review.Book.BookId).FirstOrDefault();
+            if (book != null)
+            {
+                review.Book = book;
+                databaseContext.ChangeTracker.Clear();
+                databaseContext.Attach(book);
+                databaseContext.Add(review);
                 databaseContext.SaveChanges();
                 return true;
             }
---
+++ Pages/Reviews/WriteReview.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HomeLibraryManager.Database;
+
+namespace HomeLibraryManager.Pages.Reviews
+{
+    public class WriteReviewModel : PageModel
+    {
+        private readonly BookRepository bookRepository;
+
+        public WriteReviewModel(BookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        public IActionResult OnGet(int bookID)
+        {
+            if(bookID != 0)
+            {
+                Review = new Review();
+                Review.Book = bookRepository.GetBooks().Where(x => x.BookId == bookID).FirstOrDefault();
+                return Page();
+            } 
+            else
+            {
+                return RedirectToPage("./Index");
+            }
+        }
+
+        [BindProperty]
+        public Review Review { get; set; } = default!;
+
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostReviewAsync([FromForm] Review review)
+        {
+            var bookCreated = bookRepository.CreateReview(review);
+            if(bookCreated)
+            {
+                return Redirect("/Reviews");
+            }
+            else
+            {
+                return Redirect("/Error");
+            }
+        }
+    }
+}
